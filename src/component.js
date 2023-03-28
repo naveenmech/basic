ab5ec6a3718dbd6d8dccb247f19b2f9c4e5c55f1
naveenmech <vas.naveenkumar99@gmail.com>
2023-03-28 17:28:28 +0530

    add

---
+++ src/component.js
@@ -0,0 +1,52 @@
+const Animals = [
+  {
+    id: 1,
+    name: "Tiger",
+    imgurl: https://cdn.britannica.com/40/75640-050-F894DD85/tiger-Siberian.jpg, 
+  }
+    {
+    id: 2,
+    name: "Lion",
+    imgurl:https://media.istockphoto.com/id/877369086/photo/lion-panthera-leo-10-years-old-isolated-on-white.jpg?s=612x612&w=is&k=20&c=5XzUExS8nJYT8MDiGxStc5easJ0l1_Yg041uSkduP9g= , 
+  }
+    {
+    id: 3,
+    name: "Cheetah",
+    imgurl:https://upload.wikimedia.org/wikipedia/commons/0/09/TheCheethcat.jpg , 
+  }
+    {
+    id: 4,
+    name: "Elephanat",
+    imgurl:http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcTkSca_mIEnlFmPU_hGz03cfzQekbr7xlVuMWmt_XVadGbCDaTf5bn5fEPi_6NMVHeRzmMhmF9RI9-VTDU, 
+  }
+    {
+    id: 5,
+    name: "Horse",
+    imgurl:http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRJ_M148rK8--5Yg5_0gEw49cjcIhQvvh4vv8xdgzMmNYFlqnXIHZ7BDCWEKplKd5-VcsOlHKVEfhUnrpU, 
+  }
+   {
+    id: 6,
+    name: "Giraffe",
+    imgurl:http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSh82Dm7OEK7SU7Rnv3Fa-9bi6BZrMM0NJvtm94eGajmxl7mObM7Jp9h3z5UfxaY5IsXGBoB9IX1QMVMlE, 
+  }
+     {
+    id: 7,
+    name: "Deer",
+    imgurl:https://www.woodlandtrust.org.uk/media/1278/roe-buck-in-grass-flickr-susanne-leyh.jpg, 
+  }
+     {
+    id: 8,
+    name: "Panda",
+    imgurl:https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZxRF-gnP_iW8JhWR9NbeIsvQik_OfC1X_uw&usqp=CAU, 
+  }
+     {
+    id: 9,
+    name: "Rhinoceros",
+    imgurl:https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOJ4ZcXZRzW6AJNJsXNpjUm0H8nLVtnJchsA&usqp=CAU, 
+  }
+     {
+    id: 10,
+    name: "Jaguar",
+    imgurl:https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHvlRn3NxkHB4UaEup02-b71ov1WiK55kz4A&usqp=CAU, 
+  }
+];
